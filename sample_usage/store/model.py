79b2fed1387928e1b5069244c787c52536c3e19d
Pablo Prieto <pabloprieto@live.com>
2022-02-20 14:17:18 +0100

    Addedp shopping order fields to client and product

--- sample_usage/store/model.py
+++ sample_usage/store/model.py
@@ -1,29 +1,35 @@
 from typing import *
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 
 @dataclass
 class Product:
-    id: int
+    id: int = field(init=False)
 
     name: str
     description: str
     price: float
 
+    shopping_orders: Optional[List["ShoppingOrder"]] = field(default_factory=list)
+
+
+@dataclass
+class ShoppingOrder:
+    id: int = field(init=False)
+
+    client_id: int
+    products: List[Product]
+
 
 @dataclass
 class Client:
-    id: int
+    id: int = field(init=False)
 
     first_name: str
     last_name: str
     email: str
 
+    shopping_orders: Optional[List["ShoppingOrder"]] = field(default_factory=list)
 
-@dataclass
-class ShoppingOrder:
-    id: int
 
-    client_id: int
-    products: List[Product]
